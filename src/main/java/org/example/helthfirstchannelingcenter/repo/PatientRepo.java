254f4a726b0e968a0eebe47803b33486be7e7751
misharasandalidesilva <misharasandali@gmail.com>
2025-03-31 00:10:15 +0530

    Success

--- src/main/java/org/example/helthfirstchannelingcenter/repo/PatientRepo.java
+++
@@ -1,7 +0,0 @@
-package org.example.helthfirstchannelingcenter.repo;
-
-import org.example.helthfirstchannelingcenter.entity.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientRepo extends JpaRepository<Patient, Long> {
-}
